4677c8f83570eb2a26b9219bdf1fe48bb2c836cd
mgarbazhii <m.garbazhii@gmail.com>
2024-02-25 15:26:45 +0300

    Изменения в коде, показанные в git diff

--- users/utils.py
+++ users/utils.py
@@ -5,5 +5,4 @@ from rest_framework import status
 
 def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
-    print('Это обработчик исключений')
     return Response({'detail': str(exc)}, status=status.HTTP_400_BAD_REQUEST)
